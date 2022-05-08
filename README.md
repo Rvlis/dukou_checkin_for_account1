27d288bcb5e5b2c210285277c27948ae9171d1f4
Rvlis <1065013799@qq.com>
2022-05-08 14:41:56 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# dukou_checkin_for_account1
+# Dukou每日签到+转换流量
+
+## 使用
+
+使用 github action 每日定时功能
+
+1. Fork 本项目
+
+2. 添加 Secrets
+
+   方法：Settings -> Secrets -> New repository secret
+
+    - EMAIL_KEY (邮箱,必填)
+    - PASSWD_KEY (密码,必填)
+    - SERVER_KEY ([server 酱](https://sct.ftqq.com/sendkey) key，用于将脚本结果发送 server 酱通知；选填)
+
+3. 修改 .github/workflows 定时策略 cron
+
+   参考 github action 文档 [CRON](https://docs.github.com/cn/actions/reference/events-that-trigger-workflows#scheduled-events)
+
+    ```yml
+    schedule:
+        - cron: '0 12 * * *'
+    ```
+
+## 免责
+
+本代码仅用于学习，下载后请勿用于商业用途，并且使用该脚本发生的一切后果与本人无关
---
+++ bin/dukou_chekin.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+
+class CheckinResult {
+  int ret;
+  String result;
+
+  CheckinResult({
+    required this.ret,
+    required this.result,
+  });
+
+  factory CheckinResult.fromJson(Map<String, dynamic> json) =>
+      CheckinResult(ret: json['ret'], result: json['result']);
+}
+
+class TransformResult {
+  int ret;
+  String msg;
+
+  TransformResult({
+    required this.ret,
+    required this.msg,
+  });
+
+  factory TransformResult.fromJson(Map<String, dynamic> json) =>
+      TransformResult(ret: json['ret'], msg: json['msg']);
+}
+
+void main(List<String> arguments) async {
+  var email = Platform.environment['EMAIL_KEY'];
+  var passwd = Platform.environment['PASSWD_KEY'];
+  var serverKey = Platform.environment['SERVER_KEY'];
+
+  if (email != null &&
+      passwd != null &&
+      email.isNotEmpty &&
+      passwd.isNotEmpty) {
+    var token = await login(email, passwd);
+    var checkinResult = await checkin(token);
+    var message = checkinResult.result;
+    if (checkinResult.ret == 1) {
+      TransformResult transformResult = await trafficTransform(1024, token);
+      message += '\n${transformResult.msg}';
+    }
+    // TransformResult transformResult = await trafficTransform(1024, token);
+    // message += '\n${transformResult.msg}';
+
+    if (serverKey != null && serverKey.isNotEmpty) {
+      await sendCheckinMessage(serverKey, message);
+    }
+  }
+}
+
+Future<String> login(String email, String passwd) async {
+  var response = await Dio().post(
+    'https://dukouapi.com/api/token',
+    data: {
+      'email': email,
+      'passwd': passwd,
+    },
+  );
+  var map = jsonDecode(response.data);
+  return map['token'];
+}
+
+Future<CheckinResult> checkin(String token) async {
+  var response = await Dio(BaseOptions(
+    headers: {
+      'access-token': token,
+    },
+  )).get('https://dukouapi.com/api/user/checkin');
+  print(response.data);
+  return CheckinResult.fromJson(json.decode(response.data));
+}
+
+Future<TransformResult> trafficTransform(int num, String token) async {
+  var response = await Dio(BaseOptions(headers: {'access-token': token})).get(
+    'https://dukou.dev/api/user/koukanntraffic',
+    queryParameters: {'traffic': num},
+  );
+  print(response.data);
+  return TransformResult.fromJson(json.decode(response.data));
+}
+
+Future<void> sendCheckinMessage(String serverKey, String msg) async {
+  await Dio().get(
+    'https://sctapi.ftqq.com/$serverKey.send',
+    queryParameters: {
+      'title': 'Dukou签到结果',
+      'desp': msg,
+    },
+  );
+}
